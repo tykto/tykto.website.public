74d31d0eb418e9f4e0a949f0ac6097578fbc73e5
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-04 10:28:39 +1100

    refactor: tidy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <svelte:head>
-  <meta name="description" content="Custom, affordable web design and development." />
   <meta name="author" content="Tykto" />
+  <meta name="description" content="Custom, affordable web design and development." />
   <meta
     name="keywords"
     content="web design, web development, software development, Boronia web design, Melbourne web design, custom, affordable, responsive web design, business solutions, business systems, interactive kiosk" />
