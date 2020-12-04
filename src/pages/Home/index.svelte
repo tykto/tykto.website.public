98b723db2972e1d4624bf101efe6c2791ae2e7bb
Lara Robertson <laraejrobertson@gmail.com>
2020-12-04 15:42:22 +1100

    feat: redesign site

--- src/pages/Home/index.svelte
+++ src/pages/Home/index.svelte
@@ -1,1 +1,55 @@
-Coming soon!
+<div class="grid grid-cols-1 xl:grid-cols-2 h-full">
+  <div class="flex h-screen xl:h-full tyk-bg-vanilla-light border-b-2 xl:border-b-0 border-r-0 xl:border-r-2 tyk-border-charcoal">
+    <div>
+      <div class="tyk-arrow absolute" />
+      <div
+        class="absolute tyk-bg-vanilla-light rounded-full w-32 md:w-48 lg:w-1/5 xl:w-56 tyk-w-22 mt-1 md:mt-4 lg:mt-3 xl:mt-5 tyk-2xl:mt-8 ml-1 md:ml-4 lg:ml-3 xl:ml-5 ">
+        <img src="images/logo-text.png" alt="Tykto logo" />
+        <h3 class="tyk-text-sans text-2xl sm:text-3xl lg:text-4xl hidden">Tykto</h3>
+      </div>
+    </div>
+    <div class="flex flex-1 justify-center items-center">
+      <div class="flex flex-col justify-center lg:justify-end text-5xl tyk-text-7xl tyk-text-8xl tyk-text-10xl tyk-text-sans w-3/4 tyk-h-80">
+        <div class="space-x-5"><span>Hello</span> <code class="tyk-text-purple">:)</code></div>
+        <p class="tyk-lh-tight">We specialise in business apps</p>
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col h-screen xl:h-full tyk-bg-vanilla-light">
+    <div class="flex flex-col flex-1 tyk-justify-evenly px-10 md:px-20">
+      <div>
+        <h3 class="tyk-text-sans tyk-text-purple text-3xl sm:text-4xl lg:text-5xl tyk-text-6xl">Affordable</h3>
+        <p class="text-lg sm:text-xl lg:text-2xl tyk-text-3xl">
+          Our small team, minimal overheads and years of experience gives us the agility to work with your budget
+        </p>
+      </div>
+      <div>
+        <h3 class="tyk-text-sans tyk-text-purple text-3xl sm:text-4xl lg:text-5xl tyk-text-6xl">Collaborative</h3>
+        <p class="text-lg sm:text-xl lg:text-2xl tyk-text-3xl">We're easy to deal with and we love to work <em>with</em> you to achieve your goals</p>
+      </div>
+      <div>
+        <h3 class="tyk-text-sans tyk-text-purple text-3xl sm:text-4xl lg:text-5xl tyk-text-6xl">Tailored</h3>
+        <p class="text-lg sm:text-xl lg:text-2xl tyk-text-3xl">We utilise the latest tools and technology to develop a solution to best suit your needs</p>
+      </div>
+      <p class="text-xl sm:text-2xl lg:text-3xl tyk-text-4xl">
+        For cost-effective, quality, custom, responsive applications let us know what you have in mind, and we'll make it a reality.
+      </p>
+    </div>
+    <div class="flex flex-col items-center w-full space-y-5  tyk-bg-charcoal ">
+      <h4 class="tyk-text-vanilla text-2xl sm:text-3xl lg:text-4xl tyk-text-5xl pt-5">Reach out to us!</h4>
+      <div class="flex flex-col sm:flex-row xl:flex-col tyk-2xl:flex-row justify-center sm:space-x-10">
+        <ul class="flex flex-col sm:flex-row items-center tyk-text-vanilla text-lg sm:text-xl lg:text-2xl tyk-text-3xl sm:space-x-5 lg:space-x-10 xl:space-x-5">
+          <li class=""><i class="fas fa-map-marker mr-1 lg:mr-3 xl:mr-1" />Melbourne</li>
+          <li class=""><i class="fas fa-phone mr-1 lg:mr-3 xl:mr-1" />0425807060</li>
+          <li class=""><a class="" href="#"><i class="fas fa-paper-plane mr-1 lg:mr-3 xl:mr-1" />info@tykto.com</a></li>
+        </ul>
+        <ul class="flex justify-center tyk-text-vanilla text-2xl sm:text-3xl lg:text-4xl tyk-text-5xl space-x-10">
+          <li><a class="" href="#"><i class="fab fa-facebook-square" /></a></li>
+          <li><a class="" href="#"><i class="fab fa-linkedin" /></a></li>
+          <li><a class="" href="#"><i class="fab fa-instagram-square" /></a></li>
+        </ul>
+      </div>
+    </div>
+    <p class="w-full text-right tyk-bg-charcoal tyk-text-vanilla px-5">&copy; Tykto 2020</p>
+  </div>
+</div>
